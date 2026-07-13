455e50e338a00c1927037a616a092efbaa3e0056
affan055 <8277262677ali@gmail.com>
2026-07-13 12:52:11 +0530

    registration and login done

--- books/forms.py
+++ books/forms.py
@@ -1,6 +1,8 @@
 # books/forms.py
 from django import forms
 from .models import Book
+from django.contrib.auth.models import User 
+from django.core.exceptions import ValidationError 
 
 class BookForm(forms.ModelForm):
     class Meta:
@@ -13,3 +15,43 @@ class BookForm(forms.ModelForm):
             'published_year': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter publication year'}),
             'available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
+
+class UserRegisterForm(forms.Form):
+    full_name = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Full Name'})
+    )
+    username = forms.CharField(
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Username'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter Email Address'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter Password'})
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already registered.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError("Passwords do not match.")
+        return cleaned_data
--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,13 @@
 # books/urls.py
 from django.urls import path
-from .views import BookListAPIView, add_book, book_list
+from django.contrib.auth import views as auth_views
+from .views import BookListAPIView, add_book, book_list,edit_book,register 
 
 urlpatterns = [
     path("", book_list, name="book-list"),
     path("api/", BookListAPIView.as_view(), name="book-list-api"),
     path("add/", add_book, name="add-book"),
+    path("edit/<int:id>/", edit_book, name="edit-book"),
+    path("register/", register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="books/login.html"), name="login"),
 ]
