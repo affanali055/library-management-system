92b709a79029eb1c6153d83c776b910532e0dae9
Affan Ali <8277262677ali@gmail.com>
2026-07-16 12:41:55 +0530

    everything done

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookListAPIView, add_book, book_list, edit_book, register, login_view
+from .views import BookListAPIView, add_book, book_list, edit_book, register, login_view, delete_book
 
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path("edit/<int:id>/", edit_book, name="edit-book"),
     path("register/", register, name="register"),
     path("login/", login_view, name="login"),
-]
+    path("delete/<int:id>/", delete_book, name="delete-book"),
+
+] 
